3876fb9ee9c9431f187ec048d59e0145fdcd688d
Nishadchowdhury <Nishadhj111@gmail.com>
2025-01-31 19:26:11 +0600

    store setUp finished

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.NODE_ENV === "development" ? "http://localhost:9000/" : ""
+    }),
+    tagTypes: [],
+    endpoints: (builder) => ({})
+
+})
