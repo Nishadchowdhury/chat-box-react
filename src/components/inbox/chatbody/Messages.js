41f9770f74a55b1e824966d49a8acdc7e254beb0
Nishadchowdhury <Nishadhj111@gmail.com>
2025-02-11 12:58:10 +0600

    base of the chat app is done.  socket connection will be setup later

--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -24,7 +24,6 @@ export default function Messages({ messages = [] }) {
                             message={lastMessage}
                         />
                     }
-
                     )
                 }
 
